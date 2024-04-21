afee7cecce7d05f3573c7b7d884751972f8efbc6
Zawlinhtike397 <zawlinhtike1998@gmail.com>
2024-04-21 20:04:06 +0630

    first commit

---
+++ bin/rivan_dart_learning.dart
@@ -0,0 +1,5 @@
+import 'package:rivan_dart_learning/rivan_dart_learning.dart' as rivan_dart_learning;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rivan_dart_learning.calculate()}!');
+}
